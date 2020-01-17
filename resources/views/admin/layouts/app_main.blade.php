4733af8937952676208e698af9ebbbd6e4d29164
taufik <taufik>
2020-01-17 23:55:05 +0700

    Update js

--- resources/views/admin/layouts/app_main.blade.php
+++ resources/views/admin/layouts/app_main.blade.php
@@ -239,13 +239,12 @@
         border-right: 0!important;
     }
 </style>
-<link href="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/js/select2.min.js"></script>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
-
+<link href="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.12/dist/js/select2.min.js"></script>
 <body>
     <div class="wrapper">
         <!-- Sidebar -->
