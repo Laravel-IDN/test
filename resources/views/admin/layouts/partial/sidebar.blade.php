7d2d993f897a000eed16565e253275e017f2bd4b
taufik <taufik>
2020-02-01 01:29:19 +0700

    Last Update

--- resources/views/admin/layouts/partial/sidebar.blade.php
+++ resources/views/admin/layouts/partial/sidebar.blade.php
@@ -167,12 +167,6 @@
                         Entry Meja
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="{{ route('pengunjung.index') }}"  class="nav-link {{ Str::contains(Route::currentRouteName(), 'setting.landing') ? 'active' : '' }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>
-                        Entry Pengunjung
-                    </a>
-                </li>
             </ul>
         </li>
         @endrole
